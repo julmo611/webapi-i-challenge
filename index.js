b075f1cea8fedf8a4655dd92d60cb5c73b92ee1d
Julian Moreno <julmo611@gmail.com>
2019-03-18 13:36:54 -0700

    Server running

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Here');
+  });
+
+server.listen(4000, () => {
+    console.log('\n** API up and running on port 4k **');
+});
